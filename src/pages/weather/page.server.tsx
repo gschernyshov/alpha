e2e2cfac2db865ac844682e1ecf9390d26e2ede2
gschernyshov <grchafv@yandex.ru>
2026-06-16 14:18:27 +0300

    chore: logging

--- src/pages/weather/page.server.tsx
+++ src/pages/weather/page.server.tsx
@@ -2,6 +2,8 @@ import { WeatherPage } from './UI/WeatherPage'
 import { fetchWeather } from '@/entities/weather'
 
 export const renderWeatherPage = async () => {
+  console.log('RENDER', Date.now())
+
   const weather = await fetchWeather()
 
   return <WeatherPage weather={weather} />
