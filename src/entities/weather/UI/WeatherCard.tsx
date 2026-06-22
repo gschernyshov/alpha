b8d29a7539c434ac9944ffedc2f4f659e5b8fdf3
gschernyshov <grchafv@yandex.ru>
2026-06-22 20:20:18 +0300

    feat: обновление пропсов и UI WeatherCard

--- src/entities/weather/UI/WeatherCard.tsx
+++ src/entities/weather/UI/WeatherCard.tsx
@@ -2,13 +2,23 @@ import type { ReactNode } from 'react'
 import { ShineBorder } from '@/shared/UI/shadcn/shine-border'
 
 interface WeatherCardProps {
+  id?: string
   colors: string[]
+  classNames?: string
   children: ReactNode
 }
 
-export const WeatherCard = ({ colors, children }: WeatherCardProps) => {
+export const WeatherCard = ({
+  id,
+  colors,
+  classNames = 'w-full md:w-md',
+  children,
+}: WeatherCardProps) => {
   return (
-    <div className="relative flex flex-col justify-between gap-8 w-full md:w-md p-7 card backdrop-blur-md rounded-3xl shadow-xl md:shadow-2xl dark:shadow-xs dark:shadow-foreground font-mono">
+    <div
+      id={id}
+      className={`relative flex flex-col justify-between gap-8 p-7 card backdrop-blur-md rounded-3xl shadow-xl md:shadow-2xl dark:shadow-xs dark:shadow-foreground font-mono ${classNames}`}
+    >
       <ShineBorder duration={10} shineColor={colors} />
       {children}
     </div>
