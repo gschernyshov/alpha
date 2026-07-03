d30e2b8107621bbc58d722083dddf3cd762b0bd7
gschernyshov <grchafv@yandex.ru>
2026-07-03 16:05:19 +0300

    feat: добавление состояния загрузки в WeatherCard

--- src/entities/weather/UI/WeatherCard.tsx
+++ src/entities/weather/UI/WeatherCard.tsx
@@ -1,8 +1,10 @@
 import type { ReactNode } from 'react'
+import { Spinner } from '@/shared/UI/shadcn/spinner'
 import { ShineBorder } from '@/shared/UI/shadcn/shine-border'
 
 interface WeatherCardProps {
   id?: string
+  isLoading?: boolean
   colors: string[]
   classNames?: string
   children: ReactNode
@@ -10,6 +12,7 @@ interface WeatherCardProps {
 
 export const WeatherCard = ({
   id,
+  isLoading,
   colors,
   classNames = 'w-full md:w-md',
   children,
@@ -19,6 +22,11 @@ export const WeatherCard = ({
       id={id}
       className={`relative flex flex-col justify-between gap-8 p-7 card backdrop-blur-md rounded-3xl shadow-xl md:shadow-2xl dark:shadow-xs dark:shadow-foreground font-mono ${classNames}`}
     >
+      {isLoading && (
+        <div className="absolute inset-0 flex justify-center items-center bg-background/10 backdrop-blur-xs z-50">
+          <Spinner className="size-10" />
+        </div>
+      )}
       <ShineBorder duration={10} shineColor={colors} />
       {children}
     </div>
