6001270b34783b7667c95a7e575a9d5d3bdbf5f1
gschernyshov <grchafv@yandex.ru>
2026-07-09 13:32:47 +0300

    feat: добавление активных действий в компонент AlertMessage

--- src/shared/UI/AlertMessage.tsx
+++ src/shared/UI/AlertMessage.tsx
@@ -1,21 +1,44 @@
 import { InfoIcon } from 'lucide-react'
-import { Alert, AlertTitle, AlertDescription } from '@/shared/UI/shadcn/alert'
+import {
+  Alert,
+  AlertTitle,
+  AlertDescription,
+  AlertAction,
+} from '@/shared/UI/shadcn/alert'
+import { Button } from '@/shared/UI/shadcn/button'
 
 interface AlertMessageProps {
   title?: string
   message: string
   className?: string
+  action?: {
+    label: string
+    onClick: () => void
+  }
 }
 export const AlertMessage = ({
   title,
   message,
   className,
+  action,
 }: AlertMessageProps) => {
   return (
     <Alert className={className}>
       <InfoIcon />
       <AlertTitle>{title ?? 'Предупреждение!'}</AlertTitle>
       <AlertDescription>{message}</AlertDescription>
+      {action && (
+        <AlertAction>
+          <Button
+            size="xs"
+            variant="default"
+            className="px-3 py-3 cursor-pointer"
+            onClick={action.onClick}
+          >
+            {action.label}
+          </Button>
+        </AlertAction>
+      )}
     </Alert>
   )
 }
