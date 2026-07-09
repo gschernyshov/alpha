8aab6a72be1fbf2cece5f2accbefed28743fa424
gschernyshov <grchafv@yandex.ru>
2026-07-09 13:31:15 +0300

    chore: добавление config в proxy

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,10 @@
 import { type NextRequest } from 'next/server'
 import { processRateLimit } from '@/app/middleware'
 
+export const config = {
+  matcher: ['/api/:path*'],
+}
+
 export default async function proxy(req: NextRequest) {
   return await processRateLimit(req)
 }
