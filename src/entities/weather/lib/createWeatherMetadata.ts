2e21c821928e6ca91e43cedcc6fa1a656b1d2618
gschernyshov <grchafv@yandex.ru>
2026-07-10 07:22:49 +0300

    chore: добавление логики обработки ошибок в createWeatherMetadata

--- src/entities/weather/lib/createWeatherMetadata.ts
+++ src/entities/weather/lib/createWeatherMetadata.ts
@@ -1,10 +1,21 @@
 import { fetchWeather } from '../api/fetchWeather'
 
 export const createWeatherMetadata = async () => {
-  const weather = await fetchWeather()
+  try {
+    const weather = await fetchWeather()
+
+    if (weather?.indoor) {
+      return {
+        title: `Погода | ${weather.indoor.temperature} °C`,
+        description: `Температура: ${weather.indoor.temperature} °C, влажность: ${weather.indoor.humidity} %, уровень освещения: ${weather.indoor.illumination} лк. 'Мониторинг погодных условий в реальном времени.`,
+      }
+    }
+  } catch (error) {
+    console.error('Metadata generation failed: ', error)
+  }
 
   return {
-    title: `Погода | ${weather.indoor.temperature} °C`,
-    description: `Температура: ${weather.indoor.temperature} °C, влажность: ${weather.indoor.humidity} %, уровень освещения: ${weather.indoor.illumination} лк. 'Мониторинг погодных условий в реальном времени.`,
+    title: 'Погода | Нет данных',
+    description: 'Не удалось загрузить данные погоды в реальном времени.',
   }
 }
